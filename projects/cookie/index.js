2d33a84702d74f3919a073530f4f8bd96f0a24f0
Artur Vrublevskiy <v8a8rt@gmail.com>
2021-06-14 10:47:36 +0300

    tests done....

--- projects/cookie/index.js
+++ projects/cookie/index.js
@@ -45,124 +45,93 @@ const addButton = homeworkContainer.querySelector('#add-button');
 // таблица со списком cookie
 const listTable = homeworkContainer.querySelector('#list-table tbody');
 
-/* При загрузке страницы сразу пытаемся рендерить куки */
-window.addEventListener('DOMContentLoaded', () => {
-  renderCookiesList();
-});
+const cookiesMap = getCookies();
+let filterValue = '';
+
+updateTable();
+
+function getCookies() {
+  return document.cookie
+    .split('; ')
+    .filter(Boolean)
+    .map((cookie) => cookie.match(/^([^=]+)=(.+)/))
+    .reduce((obj, [, name, value]) => {
+      obj.set(name, value);
+
+      return obj;
+    }, new Map());
+}
 
 /* При вводе в поле фильтра перерисовываем таблицу с учетом фильтра */
 filterNameInput.addEventListener('input', function () {
-  renderCookiesList(this.value);
+  filterValue = this.value;
+  updateTable();
 });
 
 /* При добавлении новой куки устанавливаем куку, перерисовываем таблицу в соответствии с фильтром*/
 addButton.addEventListener('click', () => {
-  setCookie(addNameInput.value, addValueInput.value);
-  renderCookiesList(filterNameInput.value);
-});
+  const name = encodeURIComponent(addNameInput.value.trim());
+  const value = encodeURIComponent(addValueInput.value.trim());
 
-/**
- *  Функция отрисовки таблицы.
- *  @param {data} string строка для поиска в имени или в значении куки
- *
- */
-function renderCookiesList(data = '') {
-  /* Сначала удаляем содержимое таблицы*/
-  listTable.innerHTML = '';
-  /* Отфильтровываем список кук и отрисовываем подходящие */
-  filterCookies(data).forEach((element) => {
-    createTableLine(element);
-  });
-}
-/**
- *  Функция создания DOM элементов для таблицы
- *  @param {data} object объект с данными о куке
- *
- */
-function createTableLine(data) {
-  const line = document.createElement('tr');
-  const name = document.createElement('td');
-  const value = document.createElement('td');
-  const deleteCell = document.createElement('td');
-  const deleteBtn = document.createElement('button');
-
-  name.textContent = data.name;
-  value.textContent = data.value;
-  deleteBtn.textContent = 'X';
-  deleteBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    line.remove();
-    deleteCookie(data.name);
-  });
-  deleteCell.append(deleteBtn);
-  line.append(name, value, deleteBtn);
-  listTable.append(line);
-}
-/**
- *  Фильтрация кук
- *  @param {fragment} string Строка по которой будет происходить фильтрация (частичное совпадение)
- *  @return {array} отфильтрованный массив объектов кук
- */
-function filterCookies(fragment = '') {
-  return getCookiesList().filter((element) => {
-    if (element?.name.includes(fragment) || element?.value.includes(fragment)) {
-      return element;
-    }
-  });
-}
-/**
- *  Получение кук
- *  @return {array} массив объектов кук
- */
-function getCookiesList() {
-  return document.cookie.split('; ').map((element) => {
-    const [name, value] = element.split('=');
-    if (name.length > 0) {
-      return {
-        name: name,
-        value: value,
-      };
-    }
-  });
-}
-/**
- *  Установка кук
- *  @param {name} string имя куки
- *  @param {value} string значение куки
- *  @param {options} object дополнительные параметры
- *  @return {object} объект с данными куки
- */
-function setCookie(name, value, options = {}) {
-  options = {
-    path: '/',
-    ...options,
-  };
-
-  let updatedCookie = encodeURIComponent(name) + '=' + encodeURIComponent(value);
-
-  for (const optionKey in options) {
-    updatedCookie += '; ' + optionKey;
-    const optionValue = options[optionKey];
-
-    if (optionValue !== true) {
-      updatedCookie += '=' + optionValue;
-    }
+  if (!name) {
+    return;
   }
 
-  document.cookie = updatedCookie;
+  document.cookie = `${name}=${value}`;
+  cookiesMap.set(name, value);
 
-  return {
-    name: name,
-    value: value,
-  };
-}
-/**
- *  Удаление куки
- *  @param {name} string имя куки
- *  @return {void} отрабатывает и ничего не возвращает. Жмотина а не функция
- */
-function deleteCookie(name) {
-  setCookie(name, '', {
-    'max-age': -1,
-  });
+  updateTable();
+});
+
+listTable.addEventListener('click', (e) => {
+  const { role, cookieName } = e.target.dataset;
+
+  if (role === 'remove-cookie') {
+    cookiesMap.delete(cookieName);
+    document.cookie = `${cookieName}=deleted; max-age=0`;
+    updateTable();
+  }
+});
+
+function updateTable() {
+  const fragment = document.createDocumentFragment();
+  let total = 0;
+
+  listTable.innerHTML = '';
+
+  for (const [name, value] of cookiesMap) {
+    if (
+      filterValue &&
+      !name.toLowerCase().includes(filterValue.toLowerCase()) &&
+      !value.toLowerCase().includes(filterValue.toLowerCase())
+    ) {
+      continue;
+    }
+
+    total++;
+
+    const tr = document.createElement('tr');
+    const nameTD = document.createElement('td');
+    const valueTD = document.createElement('td');
+    const removeTD = document.createElement('td');
+    const removeButton = document.createElement('button');
+
+    removeButton.dataset.role = 'remove-cookie';
+    removeButton.dataset.cookieName = name;
+    removeButton.textContent = 'Удалить';
+    nameTD.textContent = name;
+    valueTD.textContent = value;
+    valueTD.classList.add('value');
+    tr.append(nameTD, valueTD, removeTD);
+    removeTD.append(removeButton);
+
+    fragment.append(tr);
+
+    if (total) {
+      listTable.parentNode.classList.remove('hidden');
+      listTable.append(fragment);
+    } else {
+      listTable.parentNode.classList.add('hidden');
+    }
+  }
 }
