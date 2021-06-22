d94386d20ccd0b1540035341309400b669f4e3eb
Artur Vrublevskiy <v8a8rt@gmail.com>
2021-06-23 01:31:34 +0300

    Втираю какую-то дичь

--- projects/georeview/index.js
+++ projects/georeview/index.js
@@ -1,5 +1,7 @@
 const ymaps = window.ymaps;
 
+const hasOwnProperty = Object.prototype.hasOwnProperty;
+
 ymaps.ready(init);
 
 const storageName = 'geo-review';
@@ -12,14 +14,16 @@ function init() {
 
   // создаём кластер для поддержки кластеризации
   const clusterer = new ymaps.Clusterer({
-    groupByCoordinates: true,
+    // groupByCoordinates: true,
     clusterDisableClickZoom: true,
     clusterOpenBalloonOnClick: false,
   });
   // добавляем обработчик кликов по кластеру
   clusterer.events.add('click', (e) => {
-    const coords = e.get('target').geometry.getCoordinates();
-    openReviews(coords);
+    const target = e.get('target');
+    const coords = target.geometry.getCoordinates();
+
+    openReviews(coords, target);
   });
   // добавляем обработчик кликов по карте
   map.events.add('click', (e) => {
@@ -42,23 +46,78 @@ function init() {
   };
 
   // открыть отзывы
-  const openReviews = (coords) => {
-    const list = storageGetData(coords);
-    console.log('list in operReviews', list);
-    const form = createForm(coords, list);
-    openBalloon(coords, form);
+  const openReviews = (coords, targetClusterer) => {
+    // если нажатый кластер имеет несколько кластеров
+    if (hasOwnProperty.call(targetClusterer, '_clusterListeners')) {
+      // начинаем делать грязЪ
+      const places = {};
+      // перебираем кластеры
+      targetClusterer.getGeoObjects().forEach((obj) => {
+        // формируем данные под карусельку
+        const objectCoordiates = obj.geometry.getCoordinates();
+        const coordinateId = objectCoordiates.join('');
+        const coordsData = storageGetData(objectCoordiates);
+        const placeData = {
+          data: coordsData,
+          address: coordsData[0].address,
+          coordsId: coordinateId,
+        };
+        places[objectCoordiates] = placeData;
+      });
+      openBalloon(coords, createCarousel(places));
+      console.log('places', places);
+    }
+
+    // const list = storageGetData(coords);
+
+    // const form = createForm(coords, list);
+    // openBalloon(coords, form);
   };
+
+  function createCarousel(data) {
+    const container = document.createElement('div');
+    container.innerHTML = document.querySelector('#carouselTemplate').innerHTML;
+    const controlsWrap = container.querySelector('.carousel__controls-box');
+    const contentWrap = container.querySelector('.carousel__content');
+
+    for (const tab in data) {
+      const currentTab = data[tab];
+      const controlElement = document.createElement('button');
+      controlElement.classList.add('carousel__control');
+      controlElement.dataset.role = 'control-btn';
+      controlElement.textContent = currentTab.address;
+      controlsWrap.append(controlElement);
+
+      const contentTab = document.createElement('div');
+      contentTab.classList.add('carousel__tab');
+
+      console.log('data[tab].data', data[tab].data);
+      currentTab.data.forEach((review) => {
+        const reviewDiv = document.createElement('div');
+        reviewDiv.classList.add('commentsList__item', 'commentItem');
+        const { name, place, text, time } = review;
+
+        reviewDiv.innerHTML = `
+          <div class="commentItem">
+            <div class="commentItem__head">
+              <span class="commentItem__autor">${name}</span>
+              <span class="commentItem__info">${place}</span>
+              <span class="commentItem__info">${time}</span>
+            </div>
+            <div class="commentItem__text">${text}</div>
+          </div>
+          `;
+        contentTab.append(reviewDiv);
+      });
+      contentWrap.append(contentTab);
+    }
+
+    return container.innerHTML;
+  }
 
   const createPlacemark = (coords) => {
     const placemark = new ymaps.Placemark(coords);
-    // вешаем обработку кликов по точкам чтоб добавлять форму при показе балуна
-    placemark.events.add('click', (e) => {
-      const coords = e.get('target').geometry.getCoordinates();
 
-      openReviews(coords);
-    });
-
-    // после того, как плейсмарк создан, пихаем его в кластер
     clusterer.add(placemark);
   };
 
@@ -78,7 +137,7 @@ function init() {
         const div = document.createElement('div');
         div.classList.add('commentsList__item', 'commentItem');
         const { name, place, text, time } = item.review;
-        console.log(name, place, text);
+
         div.innerHTML = `
           <div class="commentItem">
             <div class="commentItem__head">
@@ -96,27 +155,24 @@ function init() {
     return container.innerHTML;
   };
 
-  const addReview = (e) => {
+  const addReview = async (e) => {
     e.preventDefault();
     if (e.target.dataset.action === 'add-review') {
       // ищем форму
       const form = document.querySelector('form[data-form=add-review]');
       // получаем её координаты
       const coords = JSON.parse(form.dataset.coords);
-      const date = new Date();
-      const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
-      const month =
-        date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
-      console.log('day', day);
-      const time = `${day}.${month}.${date.getFullYear()}`;
+
+      const address = await getAddress(coords);
       // получаем остальные данные отзыва
       const data = {
         coords: coords,
+        address: address,
         review: {
           name: form.elements.name.value,
           place: form.elements.place.value,
           text: form.elements.review.value,
-          time: time,
+          time: getDateNow(),
         },
       };
       storageAddReview(data);
@@ -130,12 +186,11 @@ function init() {
   // получить отзывы по координатам из localStorage
   const storageGetData = (coords = '') => {
     let data = JSON.parse(localStorage.getItem(storageName));
-    console.log('databefore filterred', data);
-    console.log('coords for filter', coords);
+
     if (coords.length > 0) {
       data = data.filter((element) => element.coords.join('') === coords.join(''));
     }
-    console.log('data filterred', data);
+
     return data;
   };
 
@@ -159,4 +214,22 @@ function init() {
     }
   };
   onInit();
+}
+
+function getDateNow() {
+  const date = new Date();
+  const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
+  const month =
+    date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
+
+  return `${day}.${month}.${date.getFullYear()}`;
+}
+
+async function getAddress(coords) {
+  const address = await ymaps.geocode(coords);
+  const res = address.geoObjects.get(0).getAddressLine();
+  // .then((result) => {
+  //   return result.geoObjects.get(0).getAddressLine();
+  // });
+  return res;
 }
